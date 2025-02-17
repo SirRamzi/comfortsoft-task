3bab29ad8579e13b9dddd49574e0517fc8184f06
Прокофьев Александр <aleks.aleksp@gmail.com>
2025-02-17 15:21:54 +0400

    Init project

---
+++ src/main/java/ru/prokofev/comfortsoft_task/ComfortsoftTaskApplication.java
@@ -0,0 +1,13 @@
+package ru.prokofev.comfortsoft_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComfortsoftTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComfortsoftTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/prokofev/comfortsoft_task/ComfortsoftTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.prokofev.comfortsoft_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComfortsoftTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
